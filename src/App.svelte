ded3fc08209fc270ba5ffcab35afae79790d4efb
rlfx <rlfx@realfx.rocks>
2022-09-21 10:08:07 +0200

    reset

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,45 @@
 <script>
-  import Sample from './lib/sample.svelte'
-  import Login from './lib/login.svelte'
-  import Error from './lib/error.svelte'
-  import { user, BannedTokens } from './lib/userManagement.js'
-
-  let userLocal;
-  
-  user.subscribe(value=>{userLocal=value;});
-
-  let error = false;
-  let errorDescription = "undefined";
-
-  // @ts-ignore
-  if (BannedTokens.token.indexOf(userLocal.token) !== -1) {
-    error = true;
-    errorDescription = "InvalidToken";
-  }
-
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank"> 
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
 
-{#if error}
- <Error errorCode={errorDescription} />
-{:else}
-  {#if userLocal.loggedIn == true}
-    <Sample />
-  {:else if userLocal.loggerIn == false}
-    <Login />
-  {:else}
-    <script>
+  <div class="card">
+    <Counter />
+  </div>
 
-      let SendUserBackToWhereTheyShouldBe = () => window.location.assign('http://' + window.location.host);
-      SendUserBackToWhereTheyShouldBe();
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
 
-      /*
-        Now this is functional programming!
-      */
-    </script>
-  {/if}
-{/if}
-
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
 </main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
