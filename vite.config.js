f1b078b51c362f399f5b0ca5423c665f3c8db4e0
rlfx <rlfx@realfx.rocks>
2022-10-03 17:57:57 +0200

    remove unsupported https config

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import fs from "fs";
 export default defineConfig({
   plugins: [svelte()],
   server: {
-    https: {
+    /* https: {
       key: fs.readFileSync('certs/vite.key.pem'),
       cert: fs.readFileSync('certs/vite.crt.pem'),
-    },
+    }, */
     cors: true,
   }
 })
