f13c7900b600f356906e4b0dd6536cfa0fc7179e
rlfx <rlfx@realfx.rocks>
2022-10-04 18:05:46 +0200

    Move main form to seperate file

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
+// @ts-nocheck
+
   import Feed from './lib/feed.svelte';
-  function HandleURL(event) {
-    let url = event.target.elements.URLinput.value;
-    window.location.assign('https://' + window.location.host + "/?URL=" + url);
-  }
+
+  import Form from './lib/form.svelte';
 
   // @ts-ignore
   let URLParameters = (new URL(window.location)).searchParams;
@@ -11,61 +11,18 @@
 </script>
 
 <main>
+
 <!-- This error can just be ignored, it's typescript complaining in a non-typescript file. -->
 {#if (new URL(window.location)).searchParams.has("URL")}
   <Feed />
 {:else}
-  <div>
-    <!-- Classname says what mode the user prefers, -->
-    <!-- Image suffix says what color the image hero is. -->
-    <img class="logo-dark hero" src="./logo-light@1x.png" alt="rx-//RSS">
-    <img class="logo-light hero" src="./logo-dark@1x.png" alt="rx-//RSS">
-  </div>
-  <h1>Welcome to my RSS client, Add an RSS URL below.</h1>
-  <form on:submit|preventDefault={HandleURL}>
-    <input type="url" id="URLinput" placeholder="https://example.com/index.php?show=rss">
-    <button type="submit">See the feed from URL</button>
-  </form>
-  <br>
-  <p><strong>! WARNING !</strong> It is a known bug that some websites just won't work. <strong>! WARNING !</strong></p>
+  <Form />
 {/if}
 
 </main>
 
 <style>
-  .hero {
-    display: none;
-    height: 20em;
-    padding: 1.5em;
-    will-change: filter;
-  }
 
-  @media (prefers-color-scheme: light) {
-    .logo-light {
-      display: unset;
-    }
-    .logo-dark {
-      display: none;
-    }
-  }
-  @media (prefers-color-scheme: dark) {
-    .logo-dark {
-      display: unset;
-    }
-    .logo-light {
-      display: none;
-    }
-  }
-
-  input[type=url] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    box-sizing: border-box;
-    border-radius: 1em;
-  }
-  button {
-    width: 100%;
-  }
+  /* unused, For now. */
 
 </style>
---
+++ src/lib/form.svelte
@@ -0,0 +1,73 @@
+<script>
+
+  console.log('FEEDER: ' + 'Loaded feeder\'s API!');
+
+  console.log(location.protocol)
+
+  function HandleURL(event) {
+    let url = event.target.elements.URLinput.value;
+    if (window.location.protocol == 'https:') {
+      window.location.assign('https://' + window.location.host + "/?URL=" + url);
+    } else if (window.location.protocol == 'http:') {
+      window.location.assign('http://' + window.location.host + "/?URL=" + url);
+    } else {
+      alert('You\'re not using HTTP or HTTPS, Use something more conventional for this to work.');
+    }
+  }
+
+
+</script>
+
+<div class="root">
+    <div>
+        <!--   Classname says what mode the user prefers,    -->
+        <!-- Image suffix says what color the image hero is. -->
+        <img class="logo-dark hero" src="./logo-light@1x.png" alt="rx-//RSS">
+        <img class="logo-light hero" src="./logo-dark@1x.png" alt="rx-//RSS">
+    </div>
+    <h1>Welcome to my RSS client, Add an RSS URL below.</h1>
+    <form on:submit|preventDefault={HandleURL}>
+        <input type="url" id="URLinput" placeholder="https://example.com/index.php?show=rss">
+        <button type="submit">See the feed from URL</button>
+    </form>
+    <br>
+    <p><strong>! WARNING !</strong> It is a known bug that some websites just won't work. <strong>! WARNING !</strong></p>
+</div>
+
+<style>
+    .hero {
+      display: none;
+      height: 20em;
+      padding: 1.5em;
+      will-change: filter;
+    }
+  
+    @media (prefers-color-scheme: light) {
+      .logo-light {
+        display: unset;
+      }
+      .logo-dark {
+        display: none;
+      }
+    }
+    @media (prefers-color-scheme: dark) {
+      .logo-dark {
+        display: unset;
+      }
+      .logo-light {
+        display: none;
+      }
+    }
+  
+    input[type=url] {
+      width: 100%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      box-sizing: border-box;
+      border-radius: 1em;
+    }
+    button {
+      width: 100%;
+    }
+  
+  </style>
